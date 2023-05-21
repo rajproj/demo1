064918aaf8af9929a4d764620dc2dd21a2eaae24
rajproj <crhkprj@gmail.com>
2023-05-21 23:52:57 +0530

    adding other files

--- src/main/java/com/jenkins/demo1/Demo1Application.java
+++ src/main/java/com/jenkins/demo1/Demo1Application.java
@@ -2,9 +2,10 @@ package com.jenkins.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
